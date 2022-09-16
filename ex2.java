90c39f3f47c1ef1a285df9bdb0c2bee3eac1e2c4
10 <reguldgreate@gmail.com>
2022-09-16 23:17:14 +0300

    update ex1

--- ex2.java
+++ ex2.java
@@ -1,5 +1,5 @@
 public class ex2{
 public static void main(String[] args){
-System.out.println("Java: version 11. \nJVM: HotSpot.");
+    System.out.println("Java: version 11. \nJVM: HotSpot.");
 }
 }
